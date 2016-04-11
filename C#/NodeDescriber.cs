d3d17228de59c718f5a00516ffa5db5f4f55acba
leolounz <leolounz@gmail.com>
2016-04-11 15:25:34 +1200

    Update NodeDescriber.cs

--- C#/NodeDescriber.cs
+++ C#/NodeDescriber.cs
@@ -49,6 +49,15 @@ namespace ConsoleApplication1
                 result += Subdescribe(wcn.SecondChild, count + 1);
                 result += ")";
             }
+            else if(node as ManyChildrenNode != null)
+			{
+				ManyChildrenNode mcn = (ManyChildrenNode)node;
+				List<Node> child = mcn.Children.ToList();
+				result += "new ManyChildrenNode(" + node.Name + ",";
+				foreach(var cnode in child)
+					result += Subdescribe(cnode, count + 1);
+				result += ")";
+			}
             return result;
         }
     }
